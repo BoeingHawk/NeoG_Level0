da32833e3ac580df94c59f48a6a3b0c617d50558
Pulkit Aggarwal <31651398+BoeingHawk@users.noreply.github.com>
2022-12-16 14:24:27 +0000

    complete till mark9

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // var userName = readlineSync.question('May I have your name? ');
 
 // console.log('Welcome ' + userName);
+
+/*
 let score = 0 
 
 var readlineSync = require('readline-sync');
-
 
 
 var questionOne = readlineSync.question('Are you Pulkit ? ');
@@ -31,3 +32,51 @@ if(questionTwo == asnwerTwo) {
 else console.log("Wrong Answer, " + "score" + " = 0"  )
 
 
+*/
+
+/*
+function addTwoNumbers (number1, number2){
+var sum = 0
+sum = number1 + number2
+console.log('Sum = ' + sum)  
+}
+
+addTwoNumbers(2,3)
+
+*/
+
+/*
+var readlineSync = require('readline-sync');
+
+var score = 0
+
+function quiz(question, rightAnswer) {
+
+  if (question == rightAnswer) {
+    score = score + 1
+    console.log("Right Answer, " + "score" + " = " + score)
+  }
+  else {
+    score = score - 1
+    console.log("Wrong Answer, " + "score" + " = 0")
+  }
+}
+
+
+quiz(readlineSync.question('Are you Pulkit ? '), 'yes')
+
+quiz(readlineSync.question('Is your age 25 ? '), 'yes')
+
+*/
+
+/*
+for (var i = 0; i < 5; i++){
+  console.log('Pulkit ' + i)
+}
+*/
+
+/*
+for (var i = 0; i < 5; i++)
+  console.log('*'.repeat(i))
+
+*/
